a451b65130528853572504f09ff913d5cd6cd0c6
unknown <2001020037@student.umrah.ac.id>
2022-09-17 19:27:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// Tambahkan kode JavaScript kalian di file ini
+function getFormData() {
+    const namaDepan = document.getElementById('first-name').value;
+    const namaBelakang = document.getElementById('last-name').value;
+    const asalKota = document.getElementById('city').value;
+    const kodePos = document.getElementById('zip-code').value;
+    const cek = document.getElementById('check').checked;
+
+    return{firstName:namaDepan, lastName:namaBelakang, city:asalKota, zipCode:kodePos, check:cek};
+}
+
+function isNumber (string) {
+    if (isNaN(string) == false){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function isLength(string,integer)
+{
+    if (string.length == integer)
+    {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function checkboxIsChecked(){
+    if(document.getElementById("check").checked){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function validateFormData(objek){
+    if (objek != null && isNumber(objek.zipCode) && checkboxIsChecked()){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function submit() {
+    document.getElementById('warning').style.color = '#FF0000'
+    if (!validateFormData(getFormData())) {
+        document.getElementById('warning').innerHTML = 'Periksa form anda sekali lagi'
+    } else {
+        warning.innerHTML = ''
+    }
+}
