a711dc3f596d6946173061b112924c28b6b0670c
unknown <2001020037@student.umrah.ac.id>
2022-09-19 19:46:51 +0700

    Perubahan Kedua

--- script.js
+++ script.js
@@ -1,53 +1,66 @@
 // Tambahkan kode JavaScript kalian di file ini
 function getFormData() {
-    const namaDepan = document.getElementById('first-name').value;
-    const namaBelakang = document.getElementById('last-name').value;
-    const asalKota = document.getElementById('city').value;
-    const kodePos = document.getElementById('zip-code').value;
-    const cek = document.getElementById('check').checked;
+  const namaDepan = document.getElementById("first-name").value;
+  const namaBelakang = document.getElementById("last-name").value;
+  const asalKota = document.getElementById("city").value;
+  const kodePos = document.getElementById("zip-code").value;
+  const cek = document.getElementById("check").checked;
 
-    return{firstName:namaDepan, lastName:namaBelakang, city:asalKota, zipCode:kodePos, check:cek};
+  return {
+    firstName: namaDepan,
+    lastName: namaBelakang,
+    city: asalKota,
+    zipCode: kodePos,
+    check: cek,
+  };
 }
 
-function isNumber (string) {
-    if (isNaN(string) == false){
-        return true;
-    } else{
-        return false;
-    }
+function isNumber(string) {
+  if (isNaN(string) == false) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function isLength(string,integer)
-{
-    if (string.length == integer)
-    {
-        return true;
-    } else{
-        return false;
-    }
+function isLength(string, integer) {
+  if (string.length == integer) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checkboxIsChecked(){
-    if(document.getElementById("check").checked){
-        return true;
-    } else{
-        return false;
-    }
+function checkboxIsChecked() {
+  if (document.getElementById("check").checked) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function validateFormData(objek){
-    if (objek != null && isNumber(objek.zipCode) && checkboxIsChecked()){
-        return true;
-    } else{
-        return false;
-    }
+function validateFormData(objek) {
+  if (objek != null && isNumber(objek.zipCode) && checkboxIsChecked()) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function submit() {
-    document.getElementById('warning').style.color = '#FF0000'
-    if (!validateFormData(getFormData())) {
-        document.getElementById('warning').innerHTML = 'Periksa form anda sekali lagi'
-    } else {
-        warning.innerHTML = ''
-    }
+  document.getElementById("warning").style.color = "#FF0000";
+  if (!validateFormData(getFormData())) {
+    document.getElementById("warning").innerHTML =
+      "Periksa form anda sekali lagi";
+  } else {
+    warning.innerHTML = "";
+  }
+}
+
+function resetForm() {
+  document.getElementById("first-name").value = "";
+  document.getElementById("last-name").value = "";
+  document.getElementById("city").value = "";
+  document.getElementById("zip-code").value = "";
+  document.getElementById("check").checked = false;
 }
